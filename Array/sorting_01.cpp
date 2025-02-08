e2cd935ff5d3e2788be2d1d085d4c1fe64f03f7b
ANKUSH KUMAR <77233037+ankush2992@users.noreply.github.com>
2025-02-08 11:17:21 +0000

    Updated code

---
+++ Array/sorting_01.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+void sort(int arr[] , int size){
+    int zero = 0;
+    int one = 0;
+    for(int i=0; i<size ; i++){
+        if(arr[i] == 0){
+            zero = zero + 1;
+        }
+        if(arr[i] == 1){
+            one = one + 1;
+        }
+    }
+    int i = 0;
+    for(; i<zero ; i++){
+        arr[i] = 0;git add . && git commit -m "Updated code" && git push
+
+    }
+    for(; i<size; i++){
+        arr[i] = 1 ; 
+    }
+}
+int main() {
+    int size;
+    cout<<"size"<<endl;
+    cin >> size;
+    
+    int arr[size];
+    for(int i = 0 ; i<size ; i++){
+        cout<<"Enter element at indec :"<<i<<endl;
+        cin>>arr[i];
+    }
+    
+    sort(arr , size);
+    
+    for(int i = 0 ; i<size ; i++){
+        cout<<arr[i] << " ";
+    }
+}
+    
+    
